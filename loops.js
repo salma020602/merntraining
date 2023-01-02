7cf80458120d24c3cce5669951e51c2ed3395826
mohammad salma <mdsalmas983@gmail.com>
2023-01-02 18:48:34 +0530

    js: loops with conditionals

---
+++ loops.js
@@ -0,0 +1,27 @@
+// for , while, do..while
+
+function forDemo() {
+    const start = 0;
+    const end = 10
+
+    for(let n = start; n < end; n++) {
+
+        if(n % 2 == 0)
+        console.log('iteration'+ n)
+    }
+}
+
+function whileDemo() {
+
+    let is Uploading = false
+
+    while(isUploading) {
+        console.log('Uploading Files')
+    }
+}
+
+function doWhileDemo() {
+    const isLive = falsedo {
+        console.log('Trying to Connect')
+    } while(!isLive)
+}
