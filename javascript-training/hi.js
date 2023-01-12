2aeef76f9326f29ca675a12853518cd989b9328d
mohammad salma <mdsalmas983@gmail.com>
2023-01-12 11:49:27 +0530

    updated the code

---
+++ javascript-training/hi.js
@@ -0,0 +1,15 @@
+// look at the extention *.js
+
+/**
+ * Doc Comment
+ *
+ */
+
+// console.log('Hey Hi');
+a = 10; c = 10;
+b = 20;
+console.log(a + b);
+// console.log { a + b }
+
+jkasfhs1akjdfhsd1jk = 90;
+// this is the bad way
