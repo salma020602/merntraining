94623482d73794595984b08beea36896a48bfdbb
mohammad salma <mdsalmas983@gmail.com>
2023-02-24 22:16:30 +0530

    safe commit

---
+++ advanced-javascript/animal.js
@@ -0,0 +1,42 @@
+class Animal {
+    constructor() { //state-> variables
+        this.legs = 4
+        this.eyes = 2
+    }
+
+    walk(dt) { //behaviour -> function
+        const res = 10
+        this.legs += 2
+        console.log(' ${dt} is walking')
+
+    }
+
+    run() {
+        this.legs += 5
+        console.log('Animal is Running')
+    }
+
+    sleep() {
+        this.eyes = 0
+        console.log('Animal is Sleeping')
+    }
+}
+const ab = 10
+const pq = 20
+
+
+const tiger1 = new Animal()
+tiger1.run('tiger')
+tiger1.sleep()
+tiger1.walk('tiger')
+
+const tiger2 = new Animal()
+tiger2.run('tiger')
+tiger2.sleep()
+tiger2.walk('tiger')
+
+function run(dt) {
+    console.log('You passed')
+}
+
+run('abc')
