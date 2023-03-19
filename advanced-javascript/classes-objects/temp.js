2c1f4930d13ab116bfe89ceba1791c261c368cb6
mohammad salma <mdsalmas983@gmail.com>
2023-03-19 20:08:18 +0530

    oops : inheritance

---
+++ advanced-javascript/classes-objects/temp.js
@@ -0,0 +1,21 @@
+var nm = 'pqrs'
+var date = '2022-02-22'
+console.log(nm)
+console.log(date)
+
+const utl = 10
+// objects = new + constructor()
+const utl1 = new DateUtil('3033-01-01') //object
+//console.log(one.date)
+
+// utl1.setDate('2023-03-03')
+utl1.printDare()
+utl1.plus(10)
+utl.minus(9)
+
+function abc() {
+
+}
+
+console.log(abc)
+console.log(utl1.minus)
