378e41dc26f10ea3d255ec51fe392a665de82979
mohammad salma <mdsalmas983@gmail.com>
2023-03-19 20:49:54 +0530

    opp : APIE

---
+++ advanced-javascript/inheritance/car.js
@@ -0,0 +1,55 @@
+class Car { // parent
+    constructor(speed) {
+        this.speed = speed
+    }
+
+    increaseSpeed() {
+        this.speed += 10
+    }
+
+    get crSpd() {
+        return this.speed
+    }
+
+    set crSpd(speed) {
+        this.speed = speed
+    }
+}
+
+// bmw is a car -> is a relationship
+class Bmw extends Car { // child
+     constructor(speed, isAuto) {
+        super(speed)
+
+        this.isAuto = true
+     }
+ 
+     enableAutoMode() {
+        this.isAuto = true
+    }
+}
+
+ class Audi extends Car { }
+
+ const cr = new car(12)
+ console.log(cr)
+ cr.speed = 90 // bad
+ cr.crSpd = 10 // good
+ console.log(cr.crSpd)
+
+ const bmw = new Bmw(200, true)
+ console.log(bmw)
+ bmw.speed = 10
+ bmw.increaseSpeed()
+ bmw.enableAutoMode()
+
+ function identifyCar(c = new Car()) {
+    c.increaseSpeed()
+ }
+
+ identifyCar(cr)
+ identifyCar(bmw)
+
+ console.log(cr instanceof Car) // cr is on which type ?
+ console.log(bmw instanceof car) // bmw is of which type
+
