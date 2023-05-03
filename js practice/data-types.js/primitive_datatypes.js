9c54c558d3cb72fb8d63dd88ac0f4ac6a4423488
mohammad salma <mdsalmas983@gmail.com>
2023-05-03 19:51:45 +0530

    data types: primitive-data types

---
+++ js practice/data-types.js/primitive_datatypes.js
@@ -0,0 +1,17 @@
+//nn ss bb u are primitive data types in js
+let a = null;
+let b = 123;
+let c = true;//can also be false
+let d = BigInt('123')+BigInt('123');
+let e = "salma"
+let f = Symbol("I am a nice symbol")
+let g = undefined
+console.log(a,b,c,d.e,f,g )
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+
