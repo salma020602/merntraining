207b69f258c5bc3b29283b7266f3c61ebc585cbd
mohammad salma <mdsalmas983@gmail.com>
2023-05-03 19:37:13 +0530

    variables: var- let -const.js

---
+++ js practice/variable.js/var_let_const.js
@@ -0,0 +1,15 @@
+console.log("this is mohammad salma");
+var a = 20;
+ var a= 40;//var is updated and redeclared
+ var b = 5
+
+let c = 15;
+c = 25; // let is updated but not redeclared
+
+
+const e = 'salma' // neither updated nor be redeclared
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(e)
