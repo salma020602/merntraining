8fde55e3d766b4b3821e341defa3d0e06820abf9
mohammad salma <mdsalmas983@gmail.com>
2023-05-03 20:08:26 +0530

    variables: var_const

---
+++ js practice/variable.js/let.js
@@ -0,0 +1,17 @@
+let a = 25;
+let b = "salma"
+console.log(a+b)
+
+{
+    let a = 50;
+    let b = "mohammad"
+console.log(a+b)
+
+}
+const batch = {
+    batch1 : 50,
+    batch2 :30,
+    batch3 : 55
+}
+console.log(batch["batch2"])
+  
