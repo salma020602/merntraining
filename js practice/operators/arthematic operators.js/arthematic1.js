95f620cf7d97efc59c33ced7db9f3b5df5457fac
mohammad salma <mdsalmas983@gmail.com>
2023-05-03 20:21:58 +0530

    operators:arthematic.js

---
+++ js practice/operators/arthematic operators.js/arthematic1.js
@@ -0,0 +1,12 @@
+console.log("Arthematic operators in js")
+let a = 10;
+let b = 5;
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+console.log("a%b=", a%b);
+console.log("++a=", ++a);
+console.log("a++=", a++);
+console.log("--a=", --a);
+console.log("a--=", a--);
